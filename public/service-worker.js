851f3844aa36bcc4682074dd7f64e95402999251
Dinatolemy <dina.a.tolemy@gmail.com>
2020-03-25 14:07:12 +1100

    fix offline as an app

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,10 +2,9 @@ console.log("Hello from your service worker!");
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
-    "/style.css",
-    "/index.js",
-    "/db.js",
-    "/manifest.webmanifest",
+    "style.css",
+    "index.js",
+    "db.js",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
 ];
