f011056e7d4ffccb89c185dc550b8559b610ee12
Dinatolemy <dina.a.tolemy@gmail.com>
2020-03-25 12:25:56 +1100

    heroku

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,6 +6,8 @@ const FILES_TO_CACHE = [
     "/index.js",
     "/db.js",
     "/manifest.webmanifest",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
 ];
 
 const CACHE_NAME = "static-cache-v2";
